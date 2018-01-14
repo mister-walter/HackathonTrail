7ca0d282bd0e820122e744fa2dc8106a52c2717e
Andrew Walter <mist3rwalter@gmail.com>
2018-01-13 21:38:28 -0500

    Got initial game state and rendering working using tui

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,31 @@ use std::io::prelude::*;
 use std::io::{self, Stdin, Stdout};
 use std::{thread, time};
 use std::time::{Duration, Instant};
+use std::fmt;
 extern crate rand;
 use rand::Rng;
 
-struct Menu {
+extern crate tui;
 
-}
+use tui::Terminal;
+use tui::backend::{Backend, RawBackend};
+use tui::buffer::{Buffer, Cell};
+use tui::widgets::{Widget, Block, Borders, SelectableList, Paragraph};
+use tui::layout::{Group, Size, Direction, Rect};
 
-trait DrawLayer {
-    fn draw() -> Result<(), io::Error>;
-}
-
+#[derive(Copy, Clone, Debug)]
 enum HackerSitState {
     Sitting,
     Standing
 }
 
+#[derive(Copy, Clone, Debug)]
 enum HackerMood {
     Happy,
     Sad
 }
 
+#[derive(Clone, Debug)]
 struct Hacker {
     mood: HackerMood,
     hunger: u32,
@@ -40,12 +44,8 @@ struct Hacker {
     position: (u16, u16),
 }
 
-trait Widget<W: Write> {
-    fn draw(&self, &mut W);
-}
-
-impl Hacker {
-    pub fn default() -> Hacker {
+impl Default for Hacker {
+    fn default() -> Hacker {
         Hacker {
             mood: HackerMood::Happy,
             hunger: 0,
@@ -53,42 +53,52 @@ impl Hacker {
             position: (0,0)
         }
     }
+}
 
-    fn face(&self) -> &'static str {
+impl Hacker {
+    /// Get the character representing the mood of this hacker
+    fn face(&self) -> char {
         use HackerMood::*;
         match self.mood {
-            Happy => "🙂",
-            Sad => "☹️",
+            Happy => '🙂',
+            Sad => '🙁',
         }
     }
 
-    fn chair(&self) -> &'static str {
+    /// Get the character representing the chair (or lack thereof) of this hacker
+    fn chair(&self) -> char {
         match self.sit_state {
-            HackerSitState::Sitting => "💺",
-            HackerSitState::Standing => "䷋",
+            HackerSitState::Sitting => '💺',
+            HackerSitState::Standing => '䷋',
         }
     }
 }
 
-impl <W: Write> Widget<W> for Hacker {
-    fn draw(&self, stdout: &mut W) {
-        write!(
-            stdout,
-            "{}{}{}{}",
-            cursor::Goto(self.position.0, self.position.1),
-            self.face(),
-            cursor::Goto(self.position.0, self.position.1 + 1),
-            self.chair()
-        ).unwrap();
-    }
-}
+const SPLASH_TEXT: &'static str = r#"
+ _   _            _         _   _
+| | | |          | |       | | | |
+| |_| | __ _  ___| | ____ _| |_| |__   ___  _ __
+|  _  |/ _` |/ __| |/ / _` | __| '_ \ / _ \| '_ \
+| | | | (_| | (__|   < (_| | |_| | | | (_) | | | |
+\_| |_/\__,_|\___|_|\_\__,_|\__|_| |_|\___/|_| |_|
+             _____         _ _
+            |_   _|       (_) |
+              | |_ __ __ _ _| |
+              | | '__/ _` | | |
+              | | | | (_| | | |
+              \_/_|  \__,_|_|_|
 
+          Press [spacebar] to start
+"#;
+
+#[derive(Copy, Clone, Debug)]
 enum GameStatus {
     Splash,
     Planning,
     Tables,
 }
 
+#[derive(Copy, Clone, Debug)]
 enum WeatherState {
     Clear,
     Rain,
@@ -96,6 +106,7 @@ enum WeatherState {
     Hail,
 }
 
+#[derive(Clone, Debug)]
 struct GameState {
     pub hackers: Vec<Hacker>,
     pub time: u32,
@@ -103,8 +114,8 @@ struct GameState {
     pub weather: WeatherState,
 }
 
-impl GameState {
-    pub fn new() -> GameState {
+impl Default for GameState {
+    fn default() -> GameState {
         GameState {
             hackers: vec![],
             time: 0,
@@ -114,93 +125,111 @@ impl GameState {
     }
 }
 
-struct Game<R, W: Write> {
+struct Game<R> {
     state: GameState,
     status: GameStatus,
     stdin: R,
-    stdout: W
+    term: Terminal<RawBackend>
 }
 
-impl<R, W: Write> Drop for Game<R, W> {
-    fn drop(&mut self) {
-        //write!(self.stdout, "{}{}{}", clear::All, style::Reset, cursor::Goto(1, 1)).unwrap();
-        write!(self.stdout, "{}{}", style::Reset, cursor::Goto(1, 1)).unwrap();
+impl<R> fmt::Debug for Game<R> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Game {{ state: {:?}, status: {:?} }}", self.state, self.status)
     }
 }
 
-impl<R: Iterator<Item=Result<Key, std::io::Error>>, W: Write> Game<R, W> {
+impl<R: Iterator<Item=Result<Key, std::io::Error>>> Game<R> {
     fn start(&mut self) {
         loop {
             let input = self.stdin.next();
             if input.is_some() {
                 use termion::event::Key::*;
-                match input.unwrap().unwrap() {
-                    Char('a') => {
-                        write!(self.stdout, "{}", cursor::Goto(1, 1)).unwrap();
-                        write!(self.stdout, "hi").unwrap();
+                match self.status.clone() {
+                    GameStatus::Splash => {
+                        match input.unwrap().unwrap() {
+                            Char(' ') => {
+                                self.status = GameStatus::Planning;
+                            }
+                            Ctrl('c') => { return; }
+                            _ => {}
+                        };
                     },
-                    Char('\n') => {},
-                    Char('s') => {
-                        self.state.hackers[0].sit_state = HackerSitState::Standing;
+                    GameStatus::Planning => {
+                        match input.unwrap().unwrap() {
+                            Ctrl('c') => { return; }
+                            Esc => { self.status = GameStatus::Tables; }
+                            _ => {}
+                        };
+
+
                     },
-                    Ctrl('c') => { return; }
-                    _ => {}
-                };
+                    GameStatus::Tables => {
+                        match input.unwrap().unwrap() {
+                            Char('s') => {
+                                self.state.hackers[0].sit_state = HackerSitState::Standing;
+                            },
+                            Ctrl('c') => { return; }
+                            _ => {}
+                        };
+                    }
+                }
             }
 
-            for hacker in &self.state.hackers {
-                hacker.update(&mut self.state);
-                hacker.draw(&mut self.stdout);
+            let area = self.term.size().unwrap();
+
+            match &self.status {
+                GameStatus::Splash => {
+                    Paragraph::default()
+                        .text(SPLASH_TEXT)
+                        .render(&mut self.term, &area);
+                },
+                GameStatus::Planning => {
+                    Paragraph::default()
+                        .text("Press [esc] when you're done")
+                        .render(&mut self.term, &area);
+                },
+                GameStatus::Tables => {
+                    let mut hack_view = HackathonView {
+                        hackers: &self.state.hackers
+                    };
+                    hack_view.render(&mut self.term, &area);
+                }
             }
 
-            self.stdout.flush().unwrap();
+            self.term.draw();
             thread::sleep(Duration::from_millis(100));
         }
     }
 }
-//impl<R: TermRead, W: Write> Game<R, W> {
-//    fn start(&mut self) {
-//        let interval = 100;
-//        let mut before = Instant::now();
-//        loop {
-//            let now = Instant::now();
-//            let dt = (now.duration_since(before).subsec_nanos() / 1_000_000) as u64;
-//            if dt < interval {
-//                thread::sleep(Duration::from_millis(interval - dt));
-//                continue;
-//            }
-//
-//            for hacker in &self.state.hackers {
-//                hacker.draw(&mut self.stdout);
-//            }
-//
-//            self.stdout.flush().unwrap();
-//            let input = self.stdin.next().unwrap().unwrap();
-//            use termion::event::Key::*;
-//            match &input {
-//                Char('a') => {
-//                    write!(self.stdout, "{}", cursor::Goto(1,1)).unwrap();
-//                    write!(self.stdout, "hi").unwrap();
-//                },
-//                Char('\n') => {},
-//                Ctrl('c') => {return;}
-//                _ => {}
-//            };
-//        }
-//    }
-//}
+
+/// A widget to display the hackathon table view
+struct HackathonView<'a> {
+    hackers: &'a Vec<Hacker>,
+}
+
+impl<'a> Widget for HackathonView<'a> {
+    fn draw(&mut self, area: &Rect, buf: &mut Buffer) {
+        for hacker in self.hackers {
+            buf.get_mut(hacker.position.0, hacker.position.1).set_char(hacker.face());
+            buf.get_mut(hacker.position.0, hacker.position.1+1).set_char(hacker.chair());
+        }
+    }
+}
 
 fn main() {
-    let mut stdout = std::io::stdout();
-    write!(stdout, "{}", clear::All).unwrap();
+    let backend = RawBackend::new().unwrap();
+    let mut terminal = Terminal::new(backend).unwrap();
+    terminal.clear().unwrap();
+
     let mut game = Game {
-        state: GameState::new(),
+        state: GameState::default(),
         status: GameStatus::Splash,
-        //stdin: io::stdin().keys(),
         stdin: termion::async_stdin().keys(),
-        stdout: io::stdout().into_raw_mode().unwrap()
+        term: terminal
     };
+
     let mut hacker = Hacker::default();
+    hacker.mood = HackerMood::Sad;
     hacker.position = (5, 5);
     game.state.hackers.push(hacker);
 
